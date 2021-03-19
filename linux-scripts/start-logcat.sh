991fc1bcb62494b1d04ec9e61c9e3ca17e4f2ed8
xupeng <xupeng@xiaomi.com>
2021-03-19 13:42:20 +0800

    update start-logcat.sh
    Signed-off-by: xupeng <xupeng@xiaomi.com>

--- linux-scripts/start-logcat.sh
+++ linux-scripts/start-logcat.sh
@@ -2,6 +2,9 @@
 pgrep -f "adb logcat"|xargs -i kill -9 {}
 adb logcat -G 200M
 adb logcat -c
-adb logcat > $1 &
-glogg $1
+for pid in $(adb shell pgrep -f com.android.camera)
+do
+    adb logcat --pid=$pid > $1_$pid.log &
+    glogg $1_$pid.log
+done
 
