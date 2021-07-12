28224947e118c814ac2882960f0ef00d0233ee25
xupeng <xupeng@xiaomi.com>
2021-07-12 17:08:00 +0800

    camera: Fix that the file name with spaces cannot be decompressed.
    Signed-off-by: xupeng <xupeng@xiaomi.com>

--- linux-scripts/open-bugreport.sh
+++ linux-scripts/open-bugreport.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 function ex () {
-    if [ -f $1 ]
+    if [ -f "$1" ]
         then
-            case $1 in
-            (*.tar.bz2) tar xvjf $1 ;;
-            (*.tar.gz) tar xvzf $1 ;;
-            (*.bz2) bunzip2 $1 ;;
-            (*.rar) unrar -o+ x $1 ;;
-            (*.gz) gunzip $1 ;;
-            (*.tar) tar xvf $1 ;;
-            (*.tbz2) tar xvjf $1 ;;
-            (*.tgz) tar xvzf $1 ;;
-            (*.zip) unzip -o $1 ;;
-            (*.Z) uncompress $1 ;;
-            (*.7z) 7z x $1 ;;
+            case "$1" in
+            (*.tar.bz2) tar xvjf "$1" ;;
+            (*.tar.gz) tar xvzf "$1" ;;
+            (*.bz2) bunzip2 "$1" ;;
+            (*.rar) unrar x "$1" ;;
+            (*.gz) gunzip "$1" ;;
+            (*.tar) tar xvf "$1" ;;
+            (*.tbz2) tar xvjf "$1" ;;
+            (*.tgz) tar xvzf "$1" ;;
+            (*.zip) unzip -o "$1" ;;
+            (*.Z) uncompress "$1" ;;
+            (*.7z) 7z x "$1" ;;
             (*) echo "'$1' cannot be extracted via extract" ;;
     esac
     else
@@ -21,12 +21,10 @@ function ex () {
             fi
 }
 
-filename=$(basename $1 .zip)
-mkdir -p $2/$filename 
-cd $2/$filename
-ex $1
+filename=$(basename "$1" .zip)
+mkdir -p "$2/$filename"
+cd "$2/$filename"
 find . -type f -name '*.zip' -print0|xargs -I % unzip -o %
 find . -type f -name 'bugreport*.txt' |xargs -I % glogg %
-find . -type f -name 'test*.log' |xargs -I % glogg %
-find . -type f -name 'main_log*' |xargs -I % glogg %
+#find . -type f -name 'test*.log' |xargs -I % glogg %
 
